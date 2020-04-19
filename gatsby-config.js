36d8e674d97450b0af80104ebe87236f9d53201c
jai-dewani <jai.dewani.99@gmail.com>
2020-04-19 23:03:45 +0530

    Configured empty blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Blogs By Jai-D`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jai Kumar Dewani`,
+      summary: `who is still figuring out and developing this intrest in development :p.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jai_dewani`,
     },
   },
   plugins: [
