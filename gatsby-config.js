c50b49b725d4e85dc055f64e1be29b8aaee2b3b9
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2025-10-22 00:57:12 +0530

    Add google gtag feature

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,5 +119,19 @@ module.exports = {
         icon: `src/images/Jd.jpg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-E93S06SMEK",
+        ],
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ],
 }
